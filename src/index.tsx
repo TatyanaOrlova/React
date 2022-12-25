6a2b31bc56efeb7c54f002fe9e59ffed828a275f
Tanya Orlova <tanyuhamelnik3@gmail.com>
2022-12-25 11:16:10 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = React.createElement('h1', null, 'Hello React')
+function App() {
+    return (
+        <div>
+            <h1>Hello React.js</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi tenetur consequatur cumque assumenda, excepturi, labore
+                velit sint molestias corrupti tempore vel aperiam dolorem dolore
+                ab minima, nam animi eaque minus.
+            </p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{h1}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
