d5bdac917360e4c02c4731fde80a5d8dd34f3910
Tanya Orlova <tanyuhamelnik3@gmail.com>
2022-12-25 21:32:13 +0200

    content винесли окремо

---
+++ src/Content.tsx
@@ -0,0 +1,20 @@
+const Content = () => {
+    return (
+        <>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi tenetur consequatur cumque assumenda, excepturi, labore
+                velit sint molestias corrupti tempore vel aperiam dolorem dolore
+                ab minima, nam animi eaque minus.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+                distinctio facilis suscipit, blanditiis reprehenderit unde quae
+                at ipsum possimus atque quis, fuga explicabo corporis soluta
+                impedit officiis dolores a earum.
+            </p>
+        </>
+    )
+}
+
+export default Content
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Content from './Content'
 
 type TitleProps = {
     title: string
@@ -14,31 +15,10 @@ const Title = ({ title, text = 'TEST' }: TitleProps) => {
     )
 }
 
-const Content = () => {
-    return (
-        <>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi tenetur consequatur cumque assumenda, excepturi, labore
-                velit sint molestias corrupti tempore vel aperiam dolorem dolore
-                ab minima, nam animi eaque minus.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-                distinctio facilis suscipit, blanditiis reprehenderit unde quae
-                at ipsum possimus atque quis, fuga explicabo corporis soluta
-                impedit officiis dolores a earum.
-            </p>
-        </>
-    )
-}
-
 const App = () => {
     return (
         <>
             <Title text="Hello" title="React" />
-            <Title text="Hi" title="About" />
-            <Title title="Bla-bla" />
             <Content />
         </>
     )
