806271b6a6146267a34412fd8711fdbd545e2dbd
Tanya Orlova <tanyuhamelnik3@gmail.com>
2022-12-25 11:29:37 +0200

    вивели елементи окремо

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,22 @@ const Title = () => {
     return <h1>Hello React.js</h1>
 }
 
+const Content = () => {
+    return (
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+            tenetur consequatur cumque assumenda, excepturi, labore velit sint
+            molestias corrupti tempore vel aperiam dolorem dolore ab minima, nam
+            animi eaque minus.
+        </p>
+    )
+}
+
 const App = () => {
     return (
         <div>
             <Title />
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi tenetur consequatur cumque assumenda, excepturi, labore
-                velit sint molestias corrupti tempore vel aperiam dolorem dolore
-                ab minima, nam animi eaque minus.
-            </p>
+            <Content />
         </div>
     )
 }
