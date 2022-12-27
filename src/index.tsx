f00ac6b5c91c576d819a9589cd7c8a74b4e87373
Tanya Orlova <tanyuhamelnik3@gmail.com>
2022-12-27 22:07:09 +0200

    удалили файли

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Content from './Content'
-
-type TitleProps = {
-    title: string
-    text?: string
-}
-
-const Title = ({ title, text = 'TEST' }: TitleProps) => {
-    return (
-        <h1>
-            {text} {title}
-        </h1>
-    )
-}
 
 const App = () => {
-    return (
-        <>
-            <Title text="Hello" title="React" />
-            <Content
-                p1="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ut quae, soluta totam"
-                p2="voluptates velit delectus tempora pariatur eum accusamus, fugit sequi quas culpa fuga quo dicta. Voluptas, labore laborum!"
-            />
-        </>
-    )
+    return <></>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
